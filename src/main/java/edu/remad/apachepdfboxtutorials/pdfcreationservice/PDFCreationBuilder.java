3fe033fa2704d4a46d016be68570d9cd761c9db3
RenatiusvanSander / R. Meier <remad@web.de>
2022-10-06 21:14:52 +0200

    master: pdfservice PDFCreationBuilder enhanced for document information and created document information builder

--- src/main/java/edu/remad/apachepdfboxtutorials/pdfcreationservice/PDFCreationBuilder.java
+++ src/main/java/edu/remad/apachepdfboxtutorials/pdfcreationservice/PDFCreationBuilder.java
@@ -1,7 +1,9 @@
 package edu.remad.apachepdfboxtutorials.pdfcreationservice;
 
 import java.util.ArrayList;
+import java.util.Date;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.apache.pdfbox.pdmodel.PDPage;
 
 /**
@@ -20,29 +22,67 @@ public class PDFCreationBuilder {
   private final ArrayList<PDPage> pdfPages;
 
   /**
+   * pdf document information
+   */
+  private PDDocumentInformation documentInformation;
+
+  /**
    * PDFCreationBuilder constructor
    */
   public PDFCreationBuilder() {
     this.pdfDocument = new PDDocument();
     this.pdfPages = new ArrayList<>();
+    this.documentInformation = new PDDocumentInformation();
   }
 
   /**
    * Adds page
    *
    * @param page PDF page to add to pdf document
+   * @return PDF creation builder
    */
-  public void addPage(PDPage page) {
+  public PDFCreationBuilder addPage(PDPage page) {
     this.pdfPages.add(page);
+
+    return this;
   }
 
   /**
    * Adds pages.
    *
    * @param pages PDF pages to add to document
+   * @return PDF creation builder
    */
-  public void addPages(ArrayList<PDPage> pages) {
+  public PDFCreationBuilder addPages(ArrayList<PDPage> pages) {
     this.pdfPages.addAll(pages);
+
+    return this;
+  }
+
+  /**
+   * Sets document information
+   *
+   * @param author        string-encoded author
+   * @param invoiceNumber numeric invoice number
+   * @param creator       string-encoded creator
+   * @param subject       string-encoded subject
+   * @param creationDate  date-encoded creation date
+   * @param keywords      string-encoded keywords
+   * @return PDF creation builder
+   */
+  public PDFCreationBuilder setDocumentInformation(String author, long invoiceNumber,
+      String creator,
+      String subject, Date creationDate, String keywords) {
+    this.documentInformation = new DocumentInformationBuilder()
+        .setAuthor(author)
+        .setInvoiceNumber(invoiceNumber)
+        .setCreator(creator)
+        .setSubject(subject)
+        .setCreationDate(creationDate)
+        .setKeywords(keywords)
+        .build();
+
+    return this;
   }
 
   /**
@@ -56,6 +96,8 @@ public class PDFCreationBuilder {
         this.pdfDocument.addPage(page);
       }
     }
+
+    this.pdfDocument.setDocumentInformation(this.documentInformation);
 
     return this.pdfDocument;
   }
